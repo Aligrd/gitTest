83626e5e790a787ab1d27b756c1a8e93271681f2
aligrd <mr.mirmahmodi@gmail.com>
2022-08-06 20:55:58 +0430

    added a div

---
+++ index.js
@@ -0,0 +1,13 @@
+
+
+let div = document.createElement('div')
+
+div.setAttribute('id' , 'myfirstdiv')
+
+div.style.width ='200px'
+div.style.height ='200px'
+div.style.backgroundColor ='red'
+document.body.appendChild(div)
+
+
+
